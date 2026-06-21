c82ff6ec0587a40d96fde5210aff62bb4ff76d5e
Snyder <235242029+shaikharman8814-cloud@users.noreply.github.com>
2026-06-21 15:43:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -468,4 +468,5 @@ def sample_dataset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0",port=5000)
+#we gonna have to run this to make this happenn ok 
